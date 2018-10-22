507171c2f2fbd5f790bbca3c92924e086370dd7b
Jonas Andersen <jonas.bjorge.andersen@gmail.com>
2018-10-22 14:21:11 +0200

    Almost Done

---
+++ Source/Dodger/DodgerGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DodgerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DODGER_API ADodgerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
